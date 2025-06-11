c803bd897a50aabd05278abf1efa90e6e81d6da8
DESKTOP-4D5J4IF\zackn <zacknecesito@gmail.com>
2025-06-11 18:16:29 -0500

    Add GitHub Actions deploy workflow 1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deployed to ECS via GitHub Actions!"
+    return "Deployed to ECS via GitHub Actions! 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
